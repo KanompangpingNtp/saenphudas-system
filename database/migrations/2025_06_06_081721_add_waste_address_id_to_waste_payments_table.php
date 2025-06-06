6ebfe41ac35993658d5553b3e1db6de3acad2eea
iceee-xxix <kenzo.lay1936@gmail.com>
2025-06-06 15:48:32 +0700

    update

---
+++ database/migrations/2025_06_06_081721_add_waste_address_id_to_waste_payments_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('waste_payments', function (Blueprint $table) {
+            //
+            $table->integer('waste_address_id')->nullable();
+            $table->date('end_date')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('waste_payments', function (Blueprint $table) {
+            //
+        });
+    }
+};
